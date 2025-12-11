477a1fa5519e0d308490478bd4b2e1aa20411b7d
Laxman <laxmanlaxman1629@gmail.com>
2025-12-11 19:20:03 +0530

    Fix: Configure Sequelize to use DATABASE_URL in production

--- src/config/config.js
+++ src/config/config.js
@@ -20,12 +20,14 @@ module.exports = {
         logging: false
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '5432'),
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
         logging: false
     }
 };
